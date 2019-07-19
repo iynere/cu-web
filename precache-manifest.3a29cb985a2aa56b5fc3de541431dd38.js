6fbfb38b5355ff1dfb84e303d8adc4ba9b5c0fa5
iynere <iynere@users.noreply.github.com>
2019-07-18 20:01:44 -0400

    Updates

---
+++ precache-manifest.3a29cb985a2aa56b5fc3de541431dd38.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed83a8ea5ef031221e9ea61c58f4612",
+    "url": "/cu-web/index.html"
+  },
+  {
+    "revision": "b66af798496e7e642ee5",
+    "url": "/cu-web/static/css/main.d8c9d19d.chunk.css"
+  },
+  {
+    "revision": "6b2aa20250a926ac5d3a",
+    "url": "/cu-web/static/js/2.9848d509.chunk.js"
+  },
+  {
+    "revision": "b66af798496e7e642ee5",
+    "url": "/cu-web/static/js/main.f7761e81.chunk.js"
+  },
+  {
+    "revision": "a68a51823fd3498af036",
+    "url": "/cu-web/static/js/runtime~main.fe62ff1c.js"
+  },
+  {
+    "revision": "c20c80657dcd192608d7ee9436fdfaf2",
+    "url": "/cu-web/static/media/cu-logo.c20c8065.jpg"
+  }
+]);
